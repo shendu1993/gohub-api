694c48fdd0344c347141f0d6f5d85b8f6b032224
shendu <e@onebook.club>
2022-04-06 19:42:15 +0800

    验证码库

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -1,8 +1,10 @@
 package helpers
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"reflect"
 	"time"
@@ -66,4 +68,19 @@ func GetAppPath() string {
 	}
 	filepath := dir + "\\app"
 	return filepath
+}
+
+// RandomNumber 生成长度为 length 随机数字字符串
+func RandomNumber(length int) string {
+	table := [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+	b := make([]byte, length)
+	n, err := io.ReadAtLeast(rand.Reader, b, length)
+	if n != length {
+		panic(err)
+	}
+	for i := 0; i < len(b); i++ {
+		b[i] = table[int(b[i])%len(table)]
+	}
+	return string(b)
+
 }

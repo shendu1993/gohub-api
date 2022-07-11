ca16598748fdc91888ed894b5b92ab73e4d745ed
shendu <e@onebook.club>
2022-07-11 19:49:29 +0800

    migrate fresh 命令

--- app/cmd/migrate.go
+++ app/cmd/migrate.go
@@ -7,6 +7,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
+	CmdMigrate.AddCommand(
+		CmdMigrateUp,
+		CmdMigrateRollback,
+		CmdMigrateRefresh,
+		CmdMigrateReset,
+		CmdMigrateFresh,
+	)
+}
+
+func migrator() *migrate.Migrator {
+	//注册 database/migrations 下面的迁移文件
+	migrations.Initialize()
+	// 初始化 migrator
+	return migrate.NewMigrator()
+}
+
 var CmdMigrate = &cobra.Command{
 	Use:   "migrate",
 	Short: " Run database migration",
@@ -19,30 +36,46 @@ var CmdMigrateUp = &cobra.Command{
 	Run:   runUp,
 }
 
+func runUp(cmd *cobra.Command, args []string) {
+	migrator().Up()
+}
+
 var CmdMigrateRollback = &cobra.Command{
 	Use:   "down",
 	Short: "Reverse the up command",
 	Run:   runDown,
 }
 
-func init() {
-	CmdMigrate.AddCommand(
-		CmdMigrateUp,
-		CmdMigrateRollback,
-	)
-}
-
-func migrator() *migrate.Migrator {
-	//注册 database/migrations 下面的迁移文件
-	migrations.Initialize()
-	// 初始化 migrator
-	return migrate.NewMigrator()
-}
-
-func runUp(cmd *cobra.Command, args []string) {
-	migrator().Up()
-}
-
 func runDown(cmd *cobra.Command, args []string) {
 	migrator().Rollback()
+}
+
+var CmdMigrateReset = &cobra.Command{
+	Use:   "reset",
+	Short: "Rollback all database migrations",
+	Run:   runReset,
+}
+
+func runReset(cmd *cobra.Command, args []string) {
+	migrator().Reset()
+}
+
+var CmdMigrateRefresh = &cobra.Command{
+	Use:   "refresh",
+	Short: "Reset and re-run all migrations",
+	Run:   runRefresh,
+}
+
+func runRefresh(cmd *cobra.Command, args []string) {
+	migrator().Refresh()
+}
+
+var CmdMigrateFresh = &cobra.Command{
+	Use:   "fresh",
+	Short: "Drop all tables and re-run all migrations",
+	Run:   runRFresh,
+}
+
+func runRFresh(cmd *cobra.Command, args []string) {
+	migrator().Fresh()
 }

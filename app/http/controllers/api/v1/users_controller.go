298bae4736912f4b40aff22f7f432983cbfa250f
shendu <e@onebook.club>
2022-07-25 11:43:27 +0800

    用户列表

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"gohub-api/app/models/user"
 	"gohub-api/pkg/auth"
 	"gohub-api/pkg/response"
 
@@ -15,4 +16,10 @@ type UsersController struct {
 func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 	userModel := auth.CurrentUser(c)
 	response.Data(c, userModel)
+}
+
+//Index 所有用户
+func (ctrl *UsersController) Index(c *gin.Context) {
+	data := user.All()
+	response.Data(c, data)
 }

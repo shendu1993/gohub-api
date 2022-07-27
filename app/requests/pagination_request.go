0a54dc9df8a51c28408acdde15d1f7fb800b9651
shendu <e@onebook.club>
2022-07-27 15:48:01 +0800

    优化：提示与国际化

--- app/requests/pagination_request.go
+++ app/requests/pagination_request.go
@@ -1,11 +1,14 @@
 package requests
 
 import (
+	"gohub-api/pkg/locale"
+
 	"github.com/gin-gonic/gin"
 	"github.com/thedevsaddam/govalidator"
 )
 
 type PaginationRequest struct {
+	Page    string ` valid:"page" form:"page"`
 	Sort    string ` valid:"sort" form:"sort"`
 	Order   string ` valid:"order" form:"order"`
 	PerPage string `valid:"per_page" form:"per_page"`
@@ -13,19 +16,23 @@ type PaginationRequest struct {
 
 func Pagination(data interface{}, c *gin.Context) map[string][]string {
 	rules := govalidator.MapData{
+		"page":     []string{"numeric_between:1,1000"},
 		"sort":     []string{"in:id,created_at,updated_at"},
 		"order":    []string{"in:asc,desc"},
 		"per_page": []string{"numeric_between:2,100"},
 	}
 	messages := govalidator.MapData{
+		"page": []string{
+			"numeric_between:" + locale.Translate(c, "users_validate_page"),
+		},
 		"sort": []string{
-			"in:排序字段仅支持 id,created_at,updated_at",
+			"in:" + locale.Translate(c, "users_validate_sort"),
 		},
 		"order": []string{
-			"in:排序规则仅支持 asc（正序）,desc（倒序）",
+			"in:" + locale.Translate(c, "users_validate_order"),
 		},
 		"per_page": []string{
-			"numeric_between:每页条数的值介于 2~100 之间",
+			"numeric_between:" + locale.Translate(c, "users_validate_per_page"),
 		},
 	}
 	return validate(data, rules, messages)
